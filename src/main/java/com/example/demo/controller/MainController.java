b231d0bcc314d4e377f1bc1e60558eced7637fc8
salas <vladyslav.cartel88@gmail.com>
2021-07-13 02:02:39 +0300

    try fix bag

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.User;
+import com.example.demo.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +15,16 @@ import java.util.logging.Logger;
 public class MainController  {
 
     private final Logger logger = Logger.getLogger(MainController.class.getName());
+    @Autowired
+    private UserRepository userRepository;
+
 
     @GetMapping("/")
     public String mainPage(HttpServletRequest request) {
+        User user = new User();
+        user.setName("Gaguas");
+        userRepository.save(user);    
+
         logger.info("from controller");
         String check = (String) request.getAttribute("check");
         if(check != null){
